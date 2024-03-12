41ea15eb57e6b090db8398293bcecf905e2b578f
shai9 <destronan5@gmail.com>
2024-03-12 22:15:35 +0300

    install required dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#007AFF",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
