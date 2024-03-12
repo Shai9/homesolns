db7e3da3dbf482e14705de792ec99c4978067c1b
shai9 <destronan5@gmail.com>
2024-03-12 22:34:26 +0300

    import required components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome Home</h1>
+      <Hero />
     </div>
   );
 }
