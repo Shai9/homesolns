5ec287d783e946db29e3bbc0275cb821c6588e18
shai9 <destronan5@gmail.com>
2024-03-25 04:48:07 +0300

    feat: implement remotepatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode:false,
+  images: {
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: 'ap-south-1.graphassets.com',
+        port: ''
+      }
+    ]
+  }
+};
 
 export default nextConfig;
