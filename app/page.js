9563526570370b1d4837cda8f5777d34c61e104e
shai9 <destronan5@gmail.com>
2024-03-25 04:48:51 +0300

    update main page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,29 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Hero from "./_components/Hero";
+import CategoryList from "./_components/CategoryList";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import GlobalApi from "./_services/GlobalApi";
+
 
 export default function Home() {
+
+  const [categoryList, setCategorylist] = useState([])
+  useEffect(() => {
+    getCategoryList();
+  }, [])
+  
+  const getCategoryList=()=>{
+    GlobalApi.getCategory().then(resp=>{
+      setCategorylist(resp.categories);
+    })
+  }
   return (
     <div>
       <Hero />
+
+      <CategoryList categoryList={categoryList}/>
     </div>
   );
 }
